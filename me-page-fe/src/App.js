b20f622095df24ff74a3824f1e20a0e3202e99c4
martin835 <64438132+martin835@users.noreply.github.com>
2022-07-17 22:07:30 +0200

    fonts and some basics

--- me-page-fe/src/App.js
+++ me-page-fe/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {" "}
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home" className="ml-5">
+          MARTIN KONECNY
+        </Navbar.Brand>
+        <Nav className="ml-auto mr-5">
+          <Nav.Link href="#home">HOME</Nav.Link>
+          <Nav.Link href="#features">WORK</Nav.Link>
+          <Nav.Link href="#pricing">WRITING</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Hello, I'm Martin.</h1>
+            <p>asdasdasda...</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
