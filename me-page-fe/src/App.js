3087c3304066afc690143a82cbfe1f6658c32b89
martin835 <64438132+martin835@users.noreply.github.com>
2022-07-18 11:48:11 +0200

    home page tuning

--- me-page-fe/src/App.js
+++ me-page-fe/src/App.js
@@ -1,11 +1,13 @@
 import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./App.css";
+import { RiExternalLinkLine } from "react-icons/ri";
 
 function App() {
   return (
     <div>
       {" "}
-      <Navbar bg="light" variant="light">
+      {/* <Navbar bg="light" variant="light">
         <Navbar.Brand href="#home" className="ml-5">
           MARTIN KONECNY
         </Navbar.Brand>
@@ -14,12 +16,48 @@ function App() {
           <Nav.Link href="#features">WORK</Nav.Link>
           <Nav.Link href="#pricing">WRITING</Nav.Link>
         </Nav>
-      </Navbar>
+      </Navbar> */}
       <Container>
+        <Row className="mt-4 justify-content-around nav">
+          <Col xs={12} sm={6}>
+            MARTIN KONECNY
+          </Col>
+          <Col xs={12} sm={6}>
+            <div className="float-sm-right">
+              <span className="mr-4">home </span>
+
+              <span className="mr-4">work </span>
+              <span className="mr-4">bio</span>
+            </div>
+          </Col>
+        </Row>
         <Row>
-          <Col>
-            <h1>Hello, I'm Martin.</h1>
-            <p>asdasdasda...</p>
+          <Col md={{ span: 10, offset: 1 }}>
+            <h1 className="my-5">Hello, I'm Martin.</h1>
+            <p className="text-big">
+              I am a full-stack web developer with knowledge of ReactJS, NodeJS
+              with Express, Bootstrap, MongoDB and PostgreSQL. I have
+              experimented with TypeScript, Jest and Swagger. You can read more
+              about my developer journey <Link to="/">here</Link>. I am
+              currently working for public sector organization in Slovakia
+              developing a web app for collecting feedback from the users. You
+              can check out some of my work here. If you are interested in
+              working with me, you can reach me on{" "}
+              <a href="https://www.linkedin.com/in/konecnymartin/">
+                LinkedIn{" "}
+                <sup>
+                  <RiExternalLinkLine />
+                </sup>
+              </a>{" "}
+              or shoot me an{" "}
+              <a href="mailto:mk.martinkonecny@gmail.com">
+                email{" "}
+                <sup>
+                  <RiExternalLinkLine />
+                </sup>
+              </a>
+              .
+            </p>
           </Col>
         </Row>
       </Container>
